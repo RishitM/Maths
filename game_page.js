45eb9155adfb0f22b4d91e05a5f1f5eb42eaaee8
RishitM <72683716+RishitM@users.noreply.github.com>
2021-01-10 11:11:52 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -28,5 +28,50 @@ function send() {
 	document.getElementById("number1").value = "";
 	document.getElementById("number2").value = "";
 }
+question_turn = "player1";
+answer_turn = "player2";
+
+function check()
+{
+	get_answer = document.getElementById("input_check_box").value;
+	if(get_answer == actual_answer)	
+	{
+		if(answer_turn == "player1")
+		{
+			player1_score = player1_score +1;
+		    document.getElementById("Score1").innerHTML = player1_score;
+		}
+		else 
+		{
+			player2_score = player2_score +1;
+		    document.getElementById("Score2").innerHTML = player2_score;
+		}
+	}
+	
+	if(question_turn == "player1")
+	{
+		question_turn = "player2"
+		document.getElementById("playerQ").innerHTML = "Question Turn - " + player2Name ;
+	}
+	else 
+	{
+		question_turn = "player1"
+		document.getElementById("playerQ").innerHTML = "Question Turn - " + player1Name ;
+	}
+
+	if(answer_turn == "player1")
+	{
+		answer_turn = "player2"
+		document.getElementById("playerA").innerHTML = "Answer Turn - " + player2Name ;
+	}
+	else 
+	{
+		answer_turn = "player1"
+		document.getElementById("playerA").innerHTML = "Answer Turn - " + player1Name ;
+	}
+
+    document.getElementById("output").innerHTML = "";
+}
+
 
 
