9e316f736bdef58984392fdae33836da18665581
RishitM <72683716+RishitM@users.noreply.github.com>
2021-01-10 09:50:08 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,17 +1,17 @@
-player1Name = localStorage.getItem("player1NAME");
-	player2Name = localStorage.getItem("player2NAME");
+player1Name = localStorage.getItem("Player1NAME");
+	player2Name = localStorage.getItem("Player2NAME");
 
 	player1_score = 0;
 	player2_score = 0;
 
-document.getElementById("Name1").innerHTML = player1_name + " : ";
-document.getElementById("Name2").innerHTML = player2_name + " : ";
+document.getElementById("Name1").innerHTML = player1Name + " : ";
+document.getElementById("Name2").innerHTML = player2Name + " : ";
 
 document.getElementById("Score1").innerHTML = player1_score ;
 document.getElementById("Score2").innerHTML = player2_score ;
 
-document.getElementById("player_question").innerHTML = "Question Turn - " + player1Name ;
-document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2Name ;
+document.getElementById("playerQ").innerHTML = "Question Turn - " + player1Name ;
+document.getElementById("playerA").innerHTML = "Answer Turn - " + player2Name ;
 
 function send() {
 	number1 = document.getElementById("number1").value;
